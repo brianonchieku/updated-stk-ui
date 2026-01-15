4d21583ed3651f613c8f246c71ca18bb719b8c00
machuki <brianonchikeku0@gmail.com>
2026-01-15 22:32:38 +0300

    payment provider

--- lib/models/payment_provider.dart
+++ lib/models/payment_provider.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:stkui/enums/payment_status.dart';
+import 'package:stkui/services/payment_service.dart';
 
 class PaymentProvider extends ChangeNotifier{
+
+  PaymentService _service;
+  PaymentProvider(this._service);
 
   PaymentStatus _status = PaymentStatus.idle;
   String? _errorMessage;
@@ -15,8 +19,16 @@ class PaymentProvider extends ChangeNotifier{
   }) async {
     _status = PaymentStatus.loading;
     notifyListeners();
+    try{
+      await _service.sendStkPush(phone, amount);
+      _status = PaymentStatus.success;
+    } catch(e){
+      _errorMessage = e.toString();
+      _status = PaymentStatus.failure;
+    }
+
+    notifyListeners();
 
 
   }
-
 }
