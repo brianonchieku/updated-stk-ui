1aa649791ac4b2ce82eef5443f79ec2575521ed5
machuki <brianonchikeku0@gmail.com>
2026-01-20 21:56:54 +0300

    payment provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:stkui/models/payment_provider.dart';
 import 'package:stkui/pages/home_page.dart';
+import 'package:provider/provider.dart';
+import 'package:stkui/services/payment_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +13,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MultiProvider(
+        providers: [
+          Provider<PaymentService>(create: (_) => PaymentService(),),
+          ChangeNotifierProvider(
+              create: (context) => PaymentProvider(
+                context.read<PaymentService>()
+              )
+          )
+        ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
   }
 }
